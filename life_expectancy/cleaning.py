ed2acbbe43ebebc8cd30643afb27972647421992
pedroduarte <pedro_migueld@hotmail.com>
2023-12-21 16:01:27 +0000

    refactored cleaning.py and respective tests

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -3,14 +3,33 @@ import argparse
 import pandas as pd
 
 
-def clean_data(country: str):
-    """Functions that cleans data from eu_life_expectancy_raw.tsv"""
+def load_data(data_path: str, sep: str = "\t") -> pd.DataFrame:
+    """Reads the csv from a specified path to a dataframe.
 
-    # load data
-    df_eu_life_expectancy_raw = pd.read_csv("life_expectancy/data/eu_life_expectancy_raw.tsv", sep="\t")
+    Args:
+        data_path (str): The path from which to load the data.
+        sep (str, optional): The separator to use for the file. Defaults to "\t".
+
+    Returns:
+        pd.DataFrame: The dataframe containing the data.
+    """
+    df_eu_life_expectancy_raw = pd.read_csv(data_path, sep=sep)
+    return df_eu_life_expectancy_raw
+
+
+def clean_data(data: pd.DataFrame, country: str) -> pd.DataFrame:
+    """Cleans data and filters by the specified country.
+
+    Args:
+        data (pd.DataFrame): The data to be cleaned.
+        country (str): The country to filter the data by.
+
+    Returns:
+        pd.DataFrame: The cleaned data.
+    """
 
     # unpivot to long format
-    df_eu_life_expectancy = df_eu_life_expectancy_raw.copy()
+    df_eu_life_expectancy = data.copy()
     df_eu_life_expectancy[["unit", "sex", "age", "region"]] = df_eu_life_expectancy["unit,sex,age,geo\\time"].str.split(
         ",", expand=True
     )
@@ -26,15 +45,33 @@ def clean_data(country: str):
     df_eu_life_expectancy = df_eu_life_expectancy.dropna()
 
     # filter data
-    df_pt_life_expectancy = df_eu_life_expectancy.query("region == @country")
+    df_pt_life_expectancy = df_eu_life_expectancy[df_eu_life_expectancy["region"] == country]
 
-    # export data
-    df_pt_life_expectancy.to_csv(f"life_expectancy/data/{country.lower()}_life_expectancy.csv", index=False)
+    return df_pt_life_expectancy
+
+
+def save_data(data: pd.DataFrame, path: str):
+    """Saves data to csv specified by the given path.
+
+    Args:
+        data (pd.DataFrame): The dataframe to export to csv.
+        path (str): The location to save the data to.
+    """
+    data.to_csv(path, index=False)
 
 
 if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--country", help="Specify country.", type=str, default="PT")
-    args = parser.parse_args()
 
-    clean_data(args.country)
+    # args
+    args = parser.parse_args()
+    country = args.country
+
+    # paths
+    input_data_path = "life_expectancy/data/eu_life_expectancy_raw.tsv"
+    output_data_path = f"life_expectancy/data/{country.lower()}_life_expectancy.csv"
+
+    data = load_data(input_data_path)
+    cleaned_data = clean_data(data, country)
+    save_data(cleaned_data, output_data_path)
