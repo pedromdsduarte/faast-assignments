88984ba64291e216120f0f36ed588445f79860a5
pedroduarte <pedro_migueld@hotmail.com>
2023-12-29 15:16:23 +0000

    moved data loading and saving functions to data_io

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -2,19 +2,7 @@ import argparse
 
 import pandas as pd
 
-
-def load_data(data_path: str, sep: str = "\t") -> pd.DataFrame:
-    """Reads the csv from a specified path to a dataframe.
-
-    Args:
-        data_path (str): The path from which to load the data.
-        sep (str, optional): The separator to use for the file. Defaults to "\t".
-
-    Returns:
-        pd.DataFrame: The dataframe containing the data.
-    """
-    df_eu_life_expectancy_raw = pd.read_csv(data_path, sep=sep)
-    return df_eu_life_expectancy_raw
+from life_expectancy.data_io import load_data, save_data
 
 
 def clean_data(df: pd.DataFrame, country: str) -> pd.DataFrame:
@@ -30,47 +18,29 @@ def clean_data(df: pd.DataFrame, country: str) -> pd.DataFrame:
 
     # unpivot to long format
     df_eu_life_expectancy = df.copy()
-    df_eu_life_expectancy[["unit", "sex", "age", "region"]] = df_eu_life_expectancy[
-        "unit,sex,age,geo\\time"
-    ].str.split(",", expand=True)
-    df_eu_life_expectancy = df_eu_life_expectancy.drop("unit,sex,age,geo\\time", axis=1)
-    df_eu_life_expectancy = df_eu_life_expectancy.melt(
-        id_vars=["unit", "sex", "age", "region"], var_name="year"
+    df_eu_life_expectancy[["unit", "sex", "age", "region"]] = df_eu_life_expectancy["unit,sex,age,geo\\time"].str.split(
+        ",", expand=True
     )
+    df_eu_life_expectancy = df_eu_life_expectancy.drop("unit,sex,age,geo\\time", axis=1)
+    df_eu_life_expectancy = df_eu_life_expectancy.melt(id_vars=["unit", "sex", "age", "region"], var_name="year")
 
     # ensure data types
     df_eu_life_expectancy["year"] = df_eu_life_expectancy["year"].astype(int)
-    df_eu_life_expectancy["value"] = df_eu_life_expectancy["value"].str.extract(
-        r"(\d+\.\d)"
-    )
+    df_eu_life_expectancy["value"] = df_eu_life_expectancy["value"].str.extract(r"(\d+\.\d)")
     df_eu_life_expectancy["value"] = df_eu_life_expectancy["value"].astype(float)
 
     # drop nulls
     df_eu_life_expectancy = df_eu_life_expectancy.dropna()
 
     # filter data
-    df_pt_life_expectancy = df_eu_life_expectancy[
-        df_eu_life_expectancy["region"] == country
-    ]
+    df_pt_life_expectancy = df_eu_life_expectancy[df_eu_life_expectancy["region"] == country]
 
     return df_pt_life_expectancy
 
 
-def save_data(df: pd.DataFrame, path: str):
-    """Saves data to csv specified by the given path.
-
-    Args:
-        df (pd.DataFrame): The dataframe to export to csv.
-        path (str): The location to save the data to.
-    """
-    df.to_csv(path, index=False)
-
-
 if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-c", "--country", help="Specify country.", type=str, default="PT"
-    )
+    parser.add_argument("-c", "--country", help="Specify country.", type=str, default="PT")
 
     # args
     args = parser.parse_args()
