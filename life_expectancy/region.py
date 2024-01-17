ecab955af461ed96de49a83a471fe64ecee962a6
pedroduarte <pedro_migueld@hotmail.com>
2024-01-17 17:41:19 +0000

    Added docstrings and staticmethod to the redefinition of Region._generate_next_value_

--- life_expectancy/region.py
+++ life_expectancy/region.py
@@ -3,7 +3,24 @@ from enum import Enum, auto, unique
 
 @unique
 class Region(Enum):
+    """An enumerate for available regions in the life expectancy scope."""
+
+    @staticmethod
     def _generate_next_value_(name, start, count, last_values):
+        """A redefinition of the staticmethod that is used to determine the next value returned by
+        auto, in this case the name of the country itself.
+
+        Lazily stolen from: https://docs.python.org/3/library/enum.html#enum.Enum._generate_next_value_
+
+        Args:
+            name (_type_): The name of the member being defined.
+            start (_type_): The start value for the Enum; the default is 1.
+            count (_type_): The number of members currently defined, not including this one.
+            last_values (_type_): A list of the previous values.
+
+        Returns:
+            str: The name of the region.
+        """
         return name
 
     @classmethod
